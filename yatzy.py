a9ee51f199b0f0764fc9ef78f148c25ba52e4831
MMcLarty <mmclarty@cifpfbmoll.eu>
2019-12-06 21:07:38 +0100

    Refactorizar primera función y primer caso test

--- yatzy.py
+++ yatzy.py
@@ -1,14 +1,11 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
+    def chance(*dados):
         total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+        for num in dados:
+            total += num
+            return total
 
     @staticmethod
     def yatzy(dice):
